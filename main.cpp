1620b8063ea7d38b6c1e8ceeefed81eedf73509f
Vampire016 <71385224+Vampire016@users.noreply.github.com>
2020-09-19 12:40:40 +0300

    Prototype_OS

--- main.cpp
+++ main.cpp
@@ -1,12 +1,57 @@
 #include <iostream>
+#include <string>
+
+#include<stdlib.h>				//подключает функцию system("cls");
 using namespace std;
 
 
 int main()
  {
- 	cout<<"Hello world!"<<endl;
+ 	int arr[5] = {3,5,7,6,9};
+ 	
+ 	int command;
+ 	
+ 	int value;
+ 	int indx;
+ 	
+ 	//setlocale(0, "");
  	
  	
+ 	while (true)			
+{
+	cout << "0 - Exit 1 - Read 2 - Write(not more than 5 elements) 3 - sort" << endl <<  "please, choose a command: ";
+	cin>>command;
+	system("cls");
+	
+	switch (command)
+	{
+		case 1:
+			system("cls");
+			for (int i = 0; i < 5; i++)
+			{
+				cout<<arr[i]<<"\t";
+			}
+			cout<<endl<<endl;
+		break;
+			
+		case 2:
+			cout<<"Select the index of the element to fill and its value:";
+			cin>>indx>>value;
+			
+			arr[indx] = value;
+			system("cls");
+		break;
+		
+		case 3:
+		
+		break;	
+		
+		case 0:
+			return 0;
+		break;
+		
+	}
+}
  	
 	return 0;
  }
