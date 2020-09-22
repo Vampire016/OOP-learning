f8951fb957e7982f46d66733ef2efd2ecc53f979
7Github123 <71646247+7Github123@users.noreply.github.com>
2020-09-22 15:45:56 +0300

    Foo1 v 0.05

--- Foo1.cpp
+++ Foo1.cpp
@@ -16,12 +16,15 @@ using namespace std;			//use cout/cin  (std::)
 
 void Foo1(int *arr)
 {	
+	cout<<"massive before  +1";
+	cout<< endl;
+	
 	for(int i=0; i<5; i++)
 	{
 		arr[i]++;
 	}
 	
-	cout<<"uvelichte znachenia elementov massiva na 1";
+	cout<<"uvelichenie znachenia elementov massiva na 1";
 	cout<< endl;
 }
 
