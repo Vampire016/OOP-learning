551eb6d1519253964064364c0a271bd716872ab9
Antonisko2020 <71385243+Antonisko2020@users.noreply.github.com>
2020-09-22 14:55:31 +0300

    Foo3

--- Foo3.cpp
+++ Foo3.cpp
@@ -1,30 +1,37 @@
-//print revert
+#include <iostream>
 
-//arr
+using namespace std;
 
-////////////////////////////////////////////////////////////////////////////
 
-#include <iostream>				//using cout/cin
-
-#include<stdlib.h>				//using system("cls)
-
-using namespace std;			//use cout/cin  (std::)
-
-////////////////////////////////////////////////////////////////////////////
-
-/*
-void Foo3(int *arr);
+void rev(int *arr)
 {
-   if( n==1){
-      printf("%d", arr[0])
-   }
-   else {
-      printf("%d", arr[n-1]);
-      printArray(n-1, arr);
-   }
-   
-   cout <<"Vvedite massiv: " << endl;
-   
-}
-*/
 
+int size = 5;
+
+cout <<"massiv before: \t";
+
+for (int i = 0; i < size; i++)
+{
+	cout <<arr[i] << "\t";
+}
+cout << endl;
+
+
+for(int i = 0, j = size - 1; i < j; ++i, --j)
+{
+	int t = arr[i];
+	arr[i] = arr[j];
+	arr[j] = t;
+}
+
+
+cout << "massiv after:\t";
+
+for (int i = 0; i < size; i++)
+{
+	cout << arr[i] << "\t";
+}
+
+cout << endl;
+
+}
