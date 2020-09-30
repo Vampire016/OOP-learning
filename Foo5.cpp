75f984bd3adfb30cfed162e2f4615e603f755456
Vampire016 <71385224+Vampire016@users.noreply.github.com>
2020-09-30 13:03:33 +0300

    test_11111

---
+++ Foo5.cpp
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>				//using cout/cin
+
+#include<stdlib.h>				//using system("cls)
+
+using namespace std;			//use cout/cin  (std::)
+
+////////////////////////////////////////////////////////////////////////////
+
+void Foo5(int *arr)
+{
+	int SIZE = 5;
+	
+	for(int i = 1; i<SIZE; ++i)
+	{
+		for(int j = 0; j < SIZE - i; j++)
+		{
+			if(arr[j] < arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+	
+	for(int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i];
+	}
+}
+
